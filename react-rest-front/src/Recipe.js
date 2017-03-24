881e98365815c9a50695dfadeedf8cb9dc22a022
Baxter Hall <abaxterhall@gmail.com>
2017-03-23 20:56:21 -0400

    links hopefully fixed

--- react-rest-front/src/Recipe.js
+++ react-rest-front/src/Recipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {hashHistory} from 'react-router'
 import axios from 'axios';
 import Navbar from './Navbar'
 import './index.css';
--- react-rest-front/src/UserPage.js
+++ react-rest-front/src/UserPage.js
@@ -128,6 +128,7 @@ class UserPage extends Component {
         }
         else{
             return (
+            loading =
                 <div>
                     <div className="userHeader">
                         <Navbar />
